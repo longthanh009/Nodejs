e4662601cf5b1be73deee9d435301be43d3367ec
khuatlong0902 <khuatlong0902@gmail.com>
2022-03-10 16:03:07 +0700

    update b2

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,22 +1,22 @@
 var data = [
-    {id: 1 , name : "Product1"},
-    {id: 2 , name : "Product2"}
+    { id: 1, name: "Product1" },
+    { id: 2, name: "Product2" }
 ];
-export const List = (rep,res) =>{
-    rep.json(data)
+export const List = (rep, res) => {
+    res.json(data);
 };
-export const Get =(req,res) =>{
+export const Get = (req, res) => {
     res.json(data.find(item => item.id === req.params.id));
 };
-export const Upload = (req,res) =>{
-    data.push(req.body)
+export const Upload = (req, res) => {
+    data.push(req.body);
     console.log(data);
     res.json(data)
-}
-export const Delete = (req,res) =>{
+};
+export const Delete = (req, res) => {
     res.json(data.filter(item => item.id !== req.params.id));
 };
-export const Update = (req,res) =>{
+export const Update = (req, res) => {
     const result = data.map(item => item.id === req.params.id ? req.body : item);
     res.json(data.filter(item => item.id !== req.params.id));
 };
